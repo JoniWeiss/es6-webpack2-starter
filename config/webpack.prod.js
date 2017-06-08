53bc9bc2572f219db46a98f501e376a6d3953478
Joni Weiss <me@joniweiss.com>
2017-06-08 16:20:16 -0600

    FIX: replace obsolete options

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -7,114 +7,113 @@ const srcDir = path.resolve(__dirname, '..', 'src');
 const distDir = path.resolve(__dirname, '..', 'dist');
 
 module.exports = {
-  // Where to fine the source code
-  context: srcDir,
+    // Where to fine the source code
+    context: srcDir,
 
-  // No source map for production build
-  devtool: 'hidden-source-map',
+    // No source map for production build
+    devtool: 'hidden-source-map',
 
-  entry: [
-    './index.js',
-  ],
+    entry: [
+        './index.js',
+    ],
 
-  output: {
-    // The destination file name concatenated with hash (generated whenever you change your code).
-    // The hash is really useful to let the browser knows when it should get a new bundle or use the one in cache
-    filename: 'main-[hash].js',
+    output: {
+        // The destination file name concatenated with hash (generated whenever you change your code).
+        // The hash is really useful to let the browser knows when it should get a new bundle or use the one in cache
+        filename: 'main-[hash].js',
 
-    // The destination folder where to put the output bundle
-    path: distDir,
+        // The destination folder where to put the output bundle
+        path: distDir,
 
-    // Wherever resource (css, js, img) you call <script src="..."></script>,
-    // or css, or img use this path as the root
-    publicPath: '/',
+        // Wherever resource (css, js, img) you call <script src="..."></script>,
+        // or css, or img use this path as the root
+        publicPath: '/',
 
-    // At some point you'll have to debug your code, that's why I'm giving you
-    // for free a source map file to make your life easier
-    // sourceMapFilename: 'main-[hash].map',
-  },
+        // At some point you'll have to debug your code, that's why I'm giving you
+        // for free a source map file to make your life easier
+        // sourceMapFilename: 'main-[hash].map',
+    },
 
-  // The devServer config is here to enable you to run the production build. I know you wanna see
-  // the output of this awesome config with me (Webpack 2).
-  devServer: {
-    contentBase: distDir,
-    historyApiFallback: true,
-    port: 3000,
-    compress: true,
-    inline: false
-  },
+    // The devServer config is here to enable you to run the production build. I know you wanna see
+    // the output of this awesome config with me (Webpack 2).
+    devServer: {
+        contentBase: distDir,
+        historyApiFallback: true,
+        port: 3000,
+        compress: true,
+        inline: false
+    },
 
-  module: {
-    rules: [
-      {
-        // Webpack, when walking down the tree, whenever you see `.js` file use babel to transpile
-        // the code to ES5. I don't want you to look into the node_modules folder.
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: [
-          'babel-loader',
-        ],
-      },
-      {
-        test: /\.css$/,
-        exclude: /node_modules/,
-        // It's production mode and I don't want inline CSS so put all my CSS into a file
-        loader: ExtractTextPlugin.extract({
-          fallbackLoader: 'style-loader',
-          loader: 'css-loader',
+    module: {
+        rules: [{
+                // Webpack, when walking down the tree, whenever you see `.js` file use babel to transpile
+                // the code to ES5. I don't want you to look into the node_modules folder.
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [
+                    'babel-loader',
+                ],
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                // It's production mode and I don't want inline CSS so put all my CSS into a file
+                loader: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: 'css-loader',
+                }),
+            },
+            {
+                test: /\.(jpg|jpeg|png|gif|ico|svg)$/,
+                loader: 'url-loader',
+                query: {
+                    // if less, bundle the asset inline, if greater, copy it to the dist/assets folder using file-loader
+                    limit: 10000,
+
+                    name: 'assets/[name].[hash].[ext]'
+                },
+            },
+        ]
+    },
+
+    plugins: [
+        new webpack.NamedModulesPlugin(),
+
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify('production'),
+            },
         }),
-      },
-      {
-        test: /\.(jpg|jpeg|png|gif|ico|svg)$/,
-        loader: 'url-loader',
-        query: {
-          // if less, bundle the asset inline, if greater, copy it to the dist/assets folder using file-loader
-          limit: 10000,
 
-          name: 'assets/[name].[hash].[ext]'
-        },
-      },
-    ]
-  },
+        new webpack.LoaderOptionsPlugin({
+            minimize: true,
+            debug: false
+        }),
 
-  plugins: [
-    new webpack.NamedModulesPlugin(),
+        new HtmlWebpackPlugin({
+            // where to find the html template
+            template: path.join(srcDir, 'index.html'),
 
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('production'),
-      },
-    }),
+            // where to put the generated file
+            path: distDir,
 
-    new webpack.LoaderOptionsPlugin({
-      minimize: true,
-      debug: false
-    }),
+            // the output file name
+            filename: 'index.html'
+        }),
 
-    new HtmlWebpackPlugin({
-      // where to find the html template
-      template: path.join(srcDir, 'index.html'),
+        new webpack.optimize.UglifyJsPlugin({
+            beautify: false,
+            mangle: {
+                screw_ie8: true,
+                keep_fnames: true
+            },
+            compress: {
+                screw_ie8: true
+            },
+            comments: false
+        }),
 
-      // where to put the generated file
-      path: distDir,
-
-      // the output file name
-      filename: 'index.html'
-    }),
-
-    new webpack.optimize.UglifyJsPlugin({
-      beautify: false,
-      mangle: {
-        screw_ie8: true,
-        keep_fnames: true
-      },
-      compress: {
-        screw_ie8: true
-      },
-      comments: false
-    }),
-
-    // Put all css code in this file
-    new ExtractTextPlugin('app-[hash].css'),
-  ],
+        // Put all css code in this file
+        new ExtractTextPlugin('app-[hash].css'),
+    ],
 };
